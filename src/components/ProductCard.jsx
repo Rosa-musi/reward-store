1af2f77d0f868b21ee4bc1bc82203bcadae55429
Rosa-musi <nadamejor27@hotmail.com>
2021-02-13 12:08:06 -0600

    avance product  card

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,62 @@
+import styled from 'styled-components'
+
+const MainDivProduct = styled.div`
+    display: flex;
+    width: 276px;
+    height: 276px;
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.1);
+`
+const ImageDvi = styled.div`
+//checar código de proyecto 2 para hacer el hover en la imágen
+`
+const Image = styled.img`
+    width: 252px;
+`
+const ShoppingIcon = styled.img`
+    width: 42px;
+`
+
+const NotEnCoinsDiv = styled.button`
+    width: 142px;
+    height: 42px;
+    background:#616161;
+    border-radius: 20.5px;
+    color: #fff;
+    opacity: 80%;
+    cursor: pointer;
+    outline: none;
+    font-size: 14px;
+
+    &:hover{
+        background: #0AD4FA;
+        color: #fff;
+    }
+`
+
+const Coin = styled.img`
+    width: 20px;
+`
+
+
+const Separator = styled.span`
+    height: 1px;
+    width: 228px;
+    background: #D9D9D9;
+`
+
+const Category = styled.p`
+    font-weight: 400;
+    font-size: 16px;
+    color: #A3A3A3;
+`
+
+const name = styled.p`
+    font-size: 1px;
+    color:  #616161;
+`
+
+export default function ProductCard () {
+    <MainDivProduct>
+
+    </MainDivProduct>
+}
